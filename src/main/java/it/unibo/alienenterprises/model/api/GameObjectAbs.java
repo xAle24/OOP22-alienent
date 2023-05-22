5f4a89939b15a90a5a6c79f86e71685c61cd19b9
Alessio Lizza <alessio.lizza@studio.unibo.it>
2023-05-22 15:14:46 +0200

    implemeted game object abs and hitbox

--- src/main/java/it/unibo/alienenterprises/model/api/GameObjectAbs.java
+++ src/main/java/it/unibo/alienenterprises/model/api/GameObjectAbs.java
@@ -1,5 +1,40 @@
 package it.unibo.alienenterprises.model.api;
 
+import java.util.List;
+
+import it.unibo.alienenterprises.model.api.components.Component;
+import it.unibo.alienenterprises.model.geometry.Geometry2D.Point2D;
+import it.unibo.alienenterprises.model.geometry.Geometry2D.Vector2D;
+
 public abstract class GameObjectAbs implements GameObject {
+    private Point2D position;
+    private Vector2D velocity;
+
+    @Override
+    public Type getType() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getType'");
+    }
+
+    @Override
+    public boolean isAlive() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isAlive'");
+    }
+
+    @Override
+    public Point2D getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+    @Override
+    public Vector2D getVelocity() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getVelocity'");
+    }
+
+    @Override
+    public abstract List<Component> getAllComponent();
 
 }
