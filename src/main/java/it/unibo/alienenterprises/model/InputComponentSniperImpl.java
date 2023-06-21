c6a774653e9b08a8621970797677190a845f9098
Alessio Lizza <alessio.lizza@studio.unibo.it>
2023-06-21 16:46:41 +0200

    added first implementation of movement

--- src/main/java/it/unibo/alienenterprises/model/InputComponentSniperImpl.java
+++ src/main/java/it/unibo/alienenterprises/model/InputComponentSniperImpl.java
@@ -1,5 +1,7 @@
 package it.unibo.alienenterprises.model;
 
+import java.util.Random;
+
 import it.unibo.alienenterprises.model.api.GameObject;
 import it.unibo.alienenterprises.model.api.components.InputComponent;
 import it.unibo.alienenterprises.model.geometry.Geometry2D;
@@ -8,6 +10,7 @@ import it.unibo.alienenterprises.model.geometry.Geometry2D.Vector2D;
 public class InputComponentSniperImpl implements InputComponent {
 
     Vector2D movement = new Vector2D(0, 0);
+    boolean right, up = false;
 
     @Override
     public void handle(GameObject object) {
@@ -15,8 +18,33 @@ public class InputComponentSniperImpl implements InputComponent {
         object.setVelocity(movement);
     }
     
-    public void calculateMovement(GameObject player, GameObject enemy) {
-        player.getPosition();
-        
+    public void calculateMovement(GameObject enemy, GameObject player) {
+        //movement on right/left direction
+        if (enemy.getPosition().x() > player.getPosition().x() && Math.abs(enemy.getPosition().x() - player.getPosition().x()) > 700
+            || enemy.getPosition().x() < player.getPosition().x() && Math.abs(enemy.getPosition().x() - player.getPosition().x()) > 700) {
+            right = false;
+        } else {
+            right = true;
+        }
+
+        //movement on up/down direction
+        if (enemy.getPosition().y() > player.getPosition().y() && Math.abs(enemy.getPosition().x() - player.getPosition().x()) > 700
+            || enemy.getPosition().y() < player.getPosition().y() && Math.abs(enemy.getPosition().x() - player.getPosition().x()) > 700) {
+            up = false;
+        } else {
+            up = true;
+        }
+
+        Random rnd = new Random(0);
+
+        if (right && up) {
+            movement = new Vector2D(rnd.nextInt(91), 0);
+        } else if (!right && up){
+            movement = new Vector2D(rnd.nextInt(91)+90, 0);
+        } else if (!right && !up){
+            movement = new Vector2D(rnd.nextInt(91)+180, 0);
+        } else {
+            movement = new Vector2D(rnd.nextInt(91)+270, 0);
+        }
     }
 }
