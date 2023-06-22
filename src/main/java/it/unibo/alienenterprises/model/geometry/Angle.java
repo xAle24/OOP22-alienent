2b08573f843cac9501af4d87f02ec3fca2416f3c
Galileo Foschini <galileo.foschini@studio.unibo.it>
2023-06-22 23:01:03 +0200

    created Angle class

---
+++ src/main/java/it/unibo/alienenterprises/model/geometry/Angle.java
@@ -0,0 +1,24 @@
+package it.unibo.alienenterprises.model.geometry;
+
+public class Angle {
+
+    private static final int MAX_ANGLE = 360;
+
+    private final int angle;
+
+    public Angle(final int angle) {
+        var temp = angle;
+        while(temp<0 || temp>MAX_ANGLE){
+            if(temp<0){
+                temp = MAX_ANGLE - temp;
+            } else {
+                temp = temp - MAX_ANGLE;
+            }
+        }
+        this.angle = temp;
+    }
+
+    public int getAngle() {
+        return angle;
+    }   
+}
