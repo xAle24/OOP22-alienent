f9705d0610115c4247abd5b95656193dfa9b4c7d
Galileo Foschini <galileo.foschini@studio.unibo.it>
2023-07-13 23:08:15 +0200

    Added multipply function to Vector2D

--- src/main/java/it/unibo/alienenterprises/model/geometry/Vector2D.java
+++ src/main/java/it/unibo/alienenterprises/model/geometry/Vector2D.java
@@ -85,6 +85,10 @@ public class Vector2D {
         return new Vector2D(xComp + vec.getxComp(), yComp + vec.getyComp());
     }
 
+    public Vector2D mul(final double a){
+        return new Vector2D(this.xComp*a, this.yComp*a);
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -99,17 +103,22 @@ public class Vector2D {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj){
             return true;
-        if (obj == null)
+        }
+        if (obj == null){
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()){
             return false;
+        }
         Vector2D other = (Vector2D) obj;
-        if (Double.doubleToLongBits(xComp) != Double.doubleToLongBits(other.xComp))
+        if (Double.doubleToLongBits(xComp) != Double.doubleToLongBits(other.xComp)){
             return false;
-        if (Double.doubleToLongBits(yComp) != Double.doubleToLongBits(other.yComp))
+        }
+        if (Double.doubleToLongBits(yComp) != Double.doubleToLongBits(other.yComp)){
             return false;
+        }
         return true;
     }
 
