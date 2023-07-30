d851dc3b88de33bb8dfe0c718c5e247325a16885
Galileo Foschini <galileo.foschini@studio.unibo.it>
2023-07-30 13:23:39 +0200

    renamed PlayerLoaderTest to ShipLoaderTest

--- src/test/java/it/unibo/alienenterprises/model/player/ShipLoaderTest.java
+++ src/test/java/it/unibo/alienenterprises/model/player/ShipLoaderTest.java
@@ -5,8 +5,9 @@ import org.junit.jupiter.api.Test;
 import it.unibo.alienenterprises.controller.ShipLoaderImpl;
 import it.unibo.alienenterprises.controller.api.ShipLoader;
 import it.unibo.alienenterprises.model.api.components.Component;
+import it.unibo.alienenterprises.model.impl.components.PlayerInputComponentImpl;
 
-public class PlayerLoaderTest {
+public class ShipLoaderTest {
 
     @Test
     public void loadingTest() {
@@ -16,7 +17,7 @@ public class PlayerLoaderTest {
 
         var standard = p.get("standard");
         standard.getAllComponent().stream().forEach(Component::start);
-        System.out.println(standard.getAllComponent());
+        System.out.println(standard.getComponent(PlayerInputComponentImpl.class).get().getShooterComponent());
     }
 
 }
