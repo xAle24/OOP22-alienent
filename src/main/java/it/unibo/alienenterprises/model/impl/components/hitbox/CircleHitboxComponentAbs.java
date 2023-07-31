517018d40282fcc83c0ce251d8c32ae2fc4a6686
Alessio Lizza <alessio.lizza@studio.unibo.it>
2023-07-31 07:28:33 +0200

    Added method update and setposition

--- src/main/java/it/unibo/alienenterprises/model/impl/components/hitbox/CircleHitboxComponentAbs.java
+++ src/main/java/it/unibo/alienenterprises/model/impl/components/hitbox/CircleHitboxComponentAbs.java
@@ -9,10 +9,12 @@ public abstract class CircleHitboxComponentAbs extends ComponentAbs implements H
 
     private Type objectType; 
     private Circle2D hitbox;
+    private int radius;
 
     public CircleHitboxComponentAbs(GameObject object, boolean enabled, Type objectType, int radius) {
         super(object, enabled);
         this.objectType = objectType;
+        this.radius = radius;
         this.hitbox = new Circle2D(object.getPosition(), radius);
     }
 
@@ -20,16 +22,21 @@ public abstract class CircleHitboxComponentAbs extends ComponentAbs implements H
         return hitbox;
     }
 
+    public void setPosition() {
+        this.hitbox = new Circle2D(super.getGameObject().getPosition(), radius);
+    }
+    @Override
+    public void update(double deltatime){
+        setPosition();
+    }
     @Override
     public Type getType() {
         return this.objectType;
     }
-
     @Override
     public Boolean canCollide(Type type) {
         return this.objectType == type ? false : true;
     }
-
     @Override
     public abstract void isColliding(GameObject object);
 }
--- src/main/java/it/unibo/alienenterprises/model/impl/components/hitbox/LineHitboxComponentAbs.java
+++ src/main/java/it/unibo/alienenterprises/model/impl/components/hitbox/LineHitboxComponentAbs.java
@@ -9,28 +9,33 @@ import it.unibo.alienenterprises.model.geometry.Point2D;
 public abstract class LineHitboxComponentAbs extends ComponentAbs implements HitboxComponent {
 
     private Type objectType;
-    private Line2D hitbox;
+    private Line2D line;
 
     public LineHitboxComponentAbs(GameObject object, boolean enabled, Type objectType, Point2D p1, Point2D p2) {
         super(object, enabled);
         this.objectType = objectType;
-        this.hitbox = new Line2D(p1, p2);
+        this.line = new Line2D(p1, p2);
     }
 
     public Line2D getHitbox(Point2D p1, Point2D p2){
-        return hitbox;
+        return line;
     }
 
     @Override
     public Type getType() {
         return this.objectType;
     }
-
+    @Override
+    public void update(double deltatime){
+        
+    }
     @Override
     public Boolean canCollide(Type type) {
         return this.objectType == type ? false : true;
     }
-
+    public void setPosition(Point2D p1, Point2D p2) {
+        this.hitbox = new Line2D(p1, p2);
+    }
     @Override
     public abstract void isColliding(GameObject object);
 }
