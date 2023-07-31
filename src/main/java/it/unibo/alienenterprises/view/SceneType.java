9405d7cdef883ae7dd56f1ab6aca8181c8428636
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2023-07-31 23:28:12 +0200

    Small adjustments + SceneType Adjustments to make testing possible. The cat is for testing too.

---
+++ src/main/java/it/unibo/alienenterprises/view/SceneType.java
@@ -0,0 +1,32 @@
+package it.unibo.alienenterprises.view;
+
+/**
+ * Different types of scene possible in the game.
+ */
+public enum SceneType {
+    /**
+     * Login screen.
+     */
+    LOGIN,
+
+    /**
+     * Main menu.
+     */
+    MAINMENU,
+
+    /**
+     * Game world while a {@link GameSession} has not yet reached the game over
+     * status.
+     */
+    GAMESTAGE,
+
+    /**
+     * Screen shown after the player has died.
+     */
+    GAMEOVER,
+
+    /**
+     * Shop for powerups.
+     */
+    SHOP
+}
