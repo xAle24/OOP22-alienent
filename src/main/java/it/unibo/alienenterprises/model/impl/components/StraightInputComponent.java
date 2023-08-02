8cf17f647c9604811be73da54bfc420312d61db0
Galileo Foschini <galileo.foschini@studio.unibo.it>
2023-08-02 12:46:05 +0200

    Added duplicate method to StraightInputComponent

--- src/main/java/it/unibo/alienenterprises/model/impl/components/StraightInputComponent.java
+++ src/main/java/it/unibo/alienenterprises/model/impl/components/StraightInputComponent.java
@@ -1,6 +1,9 @@
 package it.unibo.alienenterprises.model.impl.components;
 
+import java.util.Optional;
+
 import it.unibo.alienenterprises.model.api.GameObject;
+import it.unibo.alienenterprises.model.api.components.Component;
 import it.unibo.alienenterprises.model.api.components.ComponentAbs;
 import it.unibo.alienenterprises.model.api.components.InputComponent;
 
@@ -14,5 +17,9 @@ public class StraightInputComponent extends ComponentAbs implements InputCompone
     public void update(final double deltaTime) {
         getGameObject().setPosition(getGameObject().getVelocity().mul(deltaTime).translate(getGameObject().getPosition()));
     }
-    
+
+    @Override
+    public Optional<Component> duplicate(final GameObject obj) {
+        return Optional.of(new StraightInputComponent(obj, isEnabled()));
+    }
 }
