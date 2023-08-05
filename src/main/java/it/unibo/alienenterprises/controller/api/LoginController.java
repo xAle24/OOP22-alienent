c8f6e8cd7e9170c1dc43c4f094f7c941c8245546
Alessio Lizza <alessio.lizza@studio.unibo.it>
2023-08-05 16:11:00 +0200

    Added login view, TODO controller

---
+++ src/main/java/it/unibo/alienenterprises/controller/api/LoginController.java
@@ -0,0 +1,10 @@
+package it.unibo.alienenterprises.controller.api;
+/**
+ * Interface for login controller.
+ */
+public interface LoginController {
+    /**
+     * Handles login button presses.
+     */
+    void handleLogin();
+}
