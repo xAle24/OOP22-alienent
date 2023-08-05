df39597f070eb140e7f195b2439c92cc7244da45
Galileo Foschini <galileo.foschini@studio.unibo.it>
2023-08-05 16:09:05 +0200

    First Implementation of the playerMenu

---
+++ src/main/java/it/unibo/alienenterprises/controller/PlayerMenuControllerImpl.java
@@ -0,0 +1,71 @@
+package it.unibo.alienenterprises.controller;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import it.unibo.alienenterprises.model.api.GameObject;
+import it.unibo.alienenterprises.model.api.Statistic;
+import it.unibo.alienenterprises.model.api.World;
+import it.unibo.alienenterprises.model.geometry.Vector2D;
+import it.unibo.alienenterprises.model.impl.ProjectileSupplierFactoryImpl;
+import it.unibo.alienenterprises.view.PlayerInfoLoaderImpl;
+import it.unibo.alienenterprises.view.ShipInfoLoader;
+
+public class PlayerMenuControllerImpl implements PlayerMenuController {
+
+    private final World world;
+    private final Map<String, GameObject> playerMap;
+    private final ShipInfoLoader info;
+    private Optional<String> selected = Optional.empty();
+
+    public PlayerMenuControllerImpl(final World world) {
+        this.world = world;
+        this.playerMap = new ShipLoaderImpl(new ProjectileSupplierFactoryImpl(world)).loadPlayerClasses();
+        this.info = new PlayerInfoLoaderImpl(playerMap.keySet());
+        this.info.load();
+    }
+
+    @Override
+    public Set<String> getPlayerIds() {
+        return playerMap.keySet();
+    }
+
+    @Override
+    public Optional<Map<Statistic,Integer>> getStats(final String id){
+        return playerMap.containsKey(id) ? Optional.of(playerMap.get(id).getAllStats()) : Optional.empty();
+    }
+
+    @Override
+    public Optional<String> getName(final String id) {
+        return this.info.getShipName(id);
+    }
+
+    @Override
+    public Optional<String> getDescription(final String id) {
+        return this.info.getShipDescription(id);
+    }
+
+    @Override
+    public Optional<String> getSpritePath(final String id) {
+        return this.info.getShipSpriteFilePath(id);
+    }
+
+    @Override
+    public void select(final String id) {
+        if(this.playerMap.containsKey(id)){
+            this.selected = Optional.of(id);
+        }
+    }
+
+    @Override
+    public void confirmSelection() {
+        if(this.selected.isEmpty()){
+            throw new IllegalStateException("nothing has been selected");
+        }
+        final var player = this.playerMap.get(this.selected.get());
+        this.world.addGameObject(player);
+        player.setVelocity(Vector2D.fromAngleAndModule(90, player.getVelocity().getModule()));
+    }
+
+}
--- src/main/java/it/unibo/alienenterprises/view/PlayerClassMenu.java
+++ src/main/java/it/unibo/alienenterprises/view/PlayerClassMenu.java
@@ -5,66 +5,101 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import it.unibo.alienenterprises.controller.PlayerSpawnerControllerImpl;
-import it.unibo.alienenterprises.model.GameWorld;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import it.unibo.alienenterprises.controller.PlayerMenuController;
+import it.unibo.alienenterprises.model.api.Statistic;
+import it.unibo.alienenterprises.view.javafx.SceneLoader;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 public class PlayerClassMenu extends BorderPane {
 
     private static final String QUESTION = "Choose your class";
 
-    //TODO linea temporanea
-    private final PlayerSpawnerControllerImpl controller = new PlayerSpawnerControllerImpl(new GameWorld());
+    private static final int NUM_BUTTONS_RAW = 3;
+
+    private final PlayerMenuController controller;
 
     private final VBox left = new VBox();
     private final GridPane center = new GridPane();
     private final VBox bottom = new VBox();
 
-    private TextArea leftTextArea  = new TextArea("Qui ci vanno le statistiche");
-    private TextArea bottomTextArea =  new TextArea("Qui ci va la descrizione e l'immagine");
+    private Text bottomTextArea = new Text("Qui ci va la descrizione e l'immagine");
 
-    private final Map<String,Button> buttons = new HashMap<>();
+    private final List<Button> buttons = new ArrayList<>();
 
-    public BorderPane setUpPlayerClassMenu() {
+    public PlayerClassMenu(final PlayerMenuController controller, final SceneLoader sceneLoader) {
+        super();
+        this.controller = controller;
 
         final VBox up = new VBox();
         up.getChildren().add(new Text(QUESTION));
         up.setAlignment(Pos.CENTER);
         this.setTop(up);
 
-        this.leftTextArea.setMaxWidth(200);
-        this.left.getChildren().add(leftTextArea);
-        this.setLeft(this.left);
+        final BorderPane container = new BorderPane();
+
+        container.setLeft(this.left);
 
         this.setCentralPane();
-        this.setCenter(this.center);
+        buttons.get(0).fire();
+        container.setCenter(this.center);
 
         this.bottom.getChildren().add(this.bottomTextArea);
-        this.setBottom(this.bottom);
+        container.setBottom(this.bottom);
 
-        return this;
+        this.setCenter(container);
+        final Button confirmButton = new Button("Conferma");
+        confirmButton.setOnAction((e)->{
+            controller.confirmSelection();
+            sceneLoader.setScene(SceneType.GAMESTAGE);
+        });
+        this.setBottom(confirmButton);
     }
 
-    private void setCentralPane(){
-        final List<String> ids  = new ArrayList<>(controller.getIdSet());
-        System.out.println(ids);
-        final var numRaws = ids.size()/(double)3;
-        for(int i = 0; i<numRaws; i++){
-            for(int t = i*3; t<ids.size() && t<i*3+3; t++){
-                final var id = ids.get(t); 
-                final var info = controller.getInfoOf(id).get();
-                final Button button = new Button(info.getName());
-                this.buttons.put(id, button);
-                this.center.add(button, t-i*3, i);
+    private void setCentralPane() {
+        final List<String> list = controller.getPlayerIds().stream().sorted().toList();
+        int j;
+        String id;
+        for (int i = 0; i < list.size() / (double) NUM_BUTTONS_RAW; i++) {
+            j = i * NUM_BUTTONS_RAW;
+            for (int t = j; t < list.size() && t < j + NUM_BUTTONS_RAW; t++) {
+                id = list.get(t);
+                final Button button = new Button(controller.getName(id).get());
+                setAction(button, id);
+                this.buttons.add(button);
+                center.add(button, t-j, i);
             }
         }
+    }
+
+    private void setAction(final Button button, final String id){
+        button.setOnAction((event)->{
+            buttons.forEach((b)->b.setDisable(false));
+            button.setDisable(true);
+            showStats(controller.getStats(id).get());
+            bottomTextArea.setText(controller.getDescription(id).get());
+            controller.select(id);
+        });
+    }
+
+    private void showStats(final Map<Statistic,Integer> stats){
+        final GridPane pane = new GridPane();
+        pane.getColumnConstraints().add(new ColumnConstraints(120));
+        pane.getColumnConstraints().add(new ColumnConstraints(50));
+        int t = 0;
+        for(final var s : Statistic.values()){
+            pane.add(new Text(s.toString()), 0, t);
+            pane.add(new Text(stats.get(s).toString()), 1, t);
+            t++;
+        }
+        left.getChildren().clear();
+        left.getChildren().add(pane);
     }
 }
