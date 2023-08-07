44411019d355c823fb51d8aad6b0bfe259bfd5e6
Alessio Lizza <alessio.lizza@studio.unibo.it>
2023-08-07 14:38:14 +0200

    Added registration view and update login controller, created alert if account is not present

---
+++ src/main/java/it/unibo/alienenterprises/view/controllers/RegistrationController.java
@@ -0,0 +1,11 @@
+package it.unibo.alienenterprises.view.controllers;
+
+/**
+ * Interface for registration controller.
+ */
+public interface RegistrationController {
+    /**
+     * Handles register button presses.
+     */
+    void handleRegister();
+}
