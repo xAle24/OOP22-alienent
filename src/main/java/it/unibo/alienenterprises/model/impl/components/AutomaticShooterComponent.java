d0acad5373802f064ca0d6af0f0a73be429af4eb
Galileo Foschini <galileo.foschini@studio.unibo.it>
2023-08-07 15:44:59 +0200

    Implemented AutomatiShooterComponent and Fixed a problem with BasicShooterComponent

--- src/main/java/it/unibo/alienenterprises/model/impl/components/AutomaticShooterComponent.java
+++ src/main/java/it/unibo/alienenterprises/model/impl/components/AutomaticShooterComponent.java
@@ -4,51 +4,77 @@ import java.util.Optional;
 import java.util.function.Supplier;
 
 import it.unibo.alienenterprises.model.api.GameObject;
+import it.unibo.alienenterprises.model.api.Statistic;
 import it.unibo.alienenterprises.model.api.components.Component;
 import it.unibo.alienenterprises.model.api.components.ComponentAbs;
+import it.unibo.alienenterprises.model.api.components.HitboxComponent;
+import it.unibo.alienenterprises.model.api.components.ProjectileHitboxComponent;
 import it.unibo.alienenterprises.model.api.components.ShooterComponent;
+import it.unibo.alienenterprises.model.api.components.HitboxComponent.Type;
+import it.unibo.alienenterprises.model.geometry.Vector2D;
 
 public class AutomaticShooterComponent extends ComponentAbs implements ShooterComponent {
 
-    public AutomaticShooterComponent(GameObject object, boolean enabled) {
+    private int delay;
+    private int projectileSpeed;
+    private Type shooterType;
+    private int damage;
+
+    private double count = 0;
+
+    private Supplier<GameObject> shot;
+
+    private boolean mustShoot = false;
+
+    public AutomaticShooterComponent(final GameObject object, final boolean enabled, final Supplier<GameObject> shot) {
         super(object, enabled);
+        this.shot = shot;
     }
 
     @Override
     public void start() {
-        // TODO Auto-generated method stub
-        super.start();
+        this.delay = getGameObject().getStatValue(Statistic.COOLDOWN);
+        this.projectileSpeed = getGameObject().getStatValue(Statistic.PROJECTILESPEED);
+        this.shooterType = getGameObject().getComponent(HitboxComponent.class).get().getType();//TODO
+        this.damage = getGameObject().getStatValue(Statistic.DAMAGE);
     }
 
     @Override
-    public void update(double deltatime) {
-        // TODO Auto-generated method stub
-        super.update(deltatime);
+    public void update(final double deltatime) {
+        this.count = this.count + deltatime;
+        if(this.count>=this.delay || this.mustShoot){
+            var p = this.shot.get();
+            p.setStatValue(Statistic.SPEED, projectileSpeed);
+            var hb = p.getComponent(ProjectileHitboxComponent.class);
+            if(hb.isPresent()){
+                hb.get().setShooter(this.shooterType);
+            }
+            p.setStatValue(Statistic.DAMAGE, damage);
+            p.setVelocity(Vector2D.fromAngleAndModule(getGameObject().getVelocity().getAngle(), projectileSpeed));
+            p.setPosition(getGameObject().getPosition());
+            this.count = 0;
+        }
     }
 
     @Override
     public void shoot() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'shoot'");
+        this.mustShoot = true;
     }
 
     @Override
     public Supplier<GameObject> getProjectileSupplier() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getProjectileSupplier'");
+        return this.shot;
     }
 
     @Override
     public void setProjectileSupplier(Supplier<GameObject> pSupplier) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setProjectileSupplier'");
+        this.shot = pSupplier;
     }
 
 
     @Override
-    public Optional<Component> duplicate(GameObject obj) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'duplicate'");
+    public Optional<Component> duplicate(final GameObject obj) {
+        return Optional.of(new AutomaticShooterComponent(obj, isEnabled(), shot));
     }
 
 }
--- src/main/java/it/unibo/alienenterprises/model/impl/components/BasicShooterComponent.java
+++ src/main/java/it/unibo/alienenterprises/model/impl/components/BasicShooterComponent.java
@@ -7,17 +7,21 @@ import it.unibo.alienenterprises.model.api.GameObject;
 import it.unibo.alienenterprises.model.api.Statistic;
 import it.unibo.alienenterprises.model.api.components.Component;
 import it.unibo.alienenterprises.model.api.components.ComponentAbs;
+import it.unibo.alienenterprises.model.api.components.HitboxComponent;
+import it.unibo.alienenterprises.model.api.components.ProjectileHitboxComponent;
 import it.unibo.alienenterprises.model.api.components.ShooterComponent;
+import it.unibo.alienenterprises.model.api.components.HitboxComponent.Type;
 import it.unibo.alienenterprises.model.geometry.Vector2D;
 
 public class BasicShooterComponent extends ComponentAbs implements ShooterComponent {
 
     private Supplier<GameObject> shoot;
-    private double delay;
+    private int delay;
     private double counter = 0;
 
     private int damage;
     private int speed;
+    private Type shooterType;
 
     private boolean trigger = false;
 
@@ -35,12 +39,15 @@ public class BasicShooterComponent extends ComponentAbs implements ShooterCompon
             this.counter = this.counter + deltatime;
         }
         if (this.trigger && this.counter >= delay) {
-            var obj = getGameObject();
             var p = shoot.get();
             p.setStatValue(Statistic.DAMAGE, damage);
             p.setStatValue(Statistic.SPEED, speed);
-            p.setVelocity(Vector2D.fromAngleAndModule(obj.getVelocity().getAngle(), deltatime));
-            p.setPosition(obj.getPosition());
+            var hb = p.getComponent(ProjectileHitboxComponent.class);
+            if(hb.isPresent()){
+                hb.get().setShooter(this.shooterType);
+            }
+            p.setVelocity(Vector2D.fromAngleAndModule(getGameObject().getVelocity().getAngle(), speed));
+            p.setPosition(getGameObject().getPosition());
         }
     }
 
@@ -49,6 +56,7 @@ public class BasicShooterComponent extends ComponentAbs implements ShooterCompon
         this.damage = getGameObject().getStatValue(Statistic.DAMAGE);
         this.speed = getGameObject().getStatValue(Statistic.PROJECTILESPEED);
         this.delay = getGameObject().getStatValue(Statistic.COOLDOWN);
+        this.shooterType = getGameObject().getComponent(HitboxComponent.class).get().getType();//TODO
     }
 
     @Override
