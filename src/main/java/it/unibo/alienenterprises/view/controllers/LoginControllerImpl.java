a897a009c3ec9928e634388c9ac6cb111e4c2f39
Alessio Lizza <alessio.lizza@studio.unibo.it>
2023-08-08 12:48:55 +0200

    Update login and registration view

--- src/main/java/it/unibo/alienenterprises/view/controllers/LoginControllerImpl.java
+++ src/main/java/it/unibo/alienenterprises/view/controllers/LoginControllerImpl.java
@@ -1,13 +1,15 @@
-package it.unibo.alienenterprises.controller;
+package it.unibo.alienenterprises.view.controllers;
 
 import java.util.Optional;
-import it.unibo.alienenterprises.controller.api.LoginController;
+
+import it.unibo.alienenterprises.controller.Controller;
 import it.unibo.alienenterprises.model.UserAccountImpl;
 import it.unibo.alienenterprises.view.ViewType;
-import it.unibo.alienenterprises.view.controllers.InitController;
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.util.Duration;
 /**
  * Controls the login view.
  */
@@ -15,23 +17,34 @@ public class LoginControllerImpl implements LoginController,InitController {
     private Controller controller; 
     @FXML
     private TextField usernameField;
+    @FXML
     private TextField passwordField;
+    @FXML
     private Label Alert;
     @Override
     public void init(Controller controller) {
         this.controller = controller;
     }
+    @FXML
     @Override
     public void handleLogin() {
         Optional<UserAccountImpl> account = 
             controller.getUserAccountHandler().login(usernameField.getAccessibleText(), passwordField.getAccessibleText());
-        if (account.isPresent()) {
+        if (!account.isEmpty()) {
             controller.changeScene(ViewType.MAINMENU);
             //TODO
         } else {
-            Alert.setText("Account non trovato, riprovare");
+            Alert.setVisible(true);
+            PauseTransition delay = new PauseTransition(Duration.seconds(6));
+            delay.setOnFinished(e -> Alert.setVisible(false));
+            delay.play();
             usernameField.clear();
             passwordField.clear();
         }
+    }
+    @FXML
+    @Override
+    public void handleRegister() {
+        controller.changeScene(ViewType.REGISTRATION);
     }
 }
