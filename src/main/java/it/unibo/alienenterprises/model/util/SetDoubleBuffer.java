230a130e2e7c67ba4466ca9ea65de43c4e8b126e
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2023-08-09 13:46:55 +0200

    Added DoubleBuffer. Fixed ConcurrentModificationException in world update

---
+++ src/main/java/it/unibo/alienenterprises/model/util/SetDoubleBuffer.java
@@ -0,0 +1,36 @@
+package it.unibo.alienenterprises.model.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Implementation of the double buffer using a {@link HashSet}.
+ */
+public class SetDoubleBuffer<T> implements DoubleBuffer<T> {
+    private Set<T> buff;
+    private Set<T> curr;
+
+    /**
+     * Creates new instance of this class.
+     */
+    public SetDoubleBuffer() {
+        this.buff = new HashSet<>();
+        this.curr = new HashSet<>();
+    }
+
+    @Override
+    public void changeBuffer() {
+        this.curr = this.buff;
+        this.buff = new HashSet<>(this.curr);
+    }
+
+    @Override
+    public Set<T> getCurr() {
+        return this.curr;
+    }
+
+    @Override
+    public Set<T> getBuff() {
+        return this.buff;
+    }
+}
