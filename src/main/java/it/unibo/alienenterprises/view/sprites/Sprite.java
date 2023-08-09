47ef9252ee18511df673bea32bbda4aacbb5861c
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2023-08-09 17:41:21 +0200

    Modified Sprite and CanvasPainter  added css for gamestage

--- src/main/java/it/unibo/alienenterprises/view/sprites/Sprite.java
+++ src/main/java/it/unibo/alienenterprises/view/sprites/Sprite.java
@@ -5,19 +5,23 @@ import it.unibo.alienenterprises.controller.ImageLoaderImpl;
 import it.unibo.alienenterprises.model.geometry.Point2D;
 import it.unibo.alienenterprises.model.geometry.Vector2D;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 /**
  * Sprite class that contains the node that will be drawn on the {@link Canvas}
  */
 public final class Sprite {
+    public static final ImageLoader IMG_LOADER = new ImageLoaderImpl();
     private ImageView image;
 
     public Sprite(String objID, Point2D pos, Vector2D angle) {
-        ImageLoader loader = new ImageLoaderImpl();
-        this.image = new ImageView(loader.getSpriteFilePathOf(objID).get());
-        this.image.setFitHeight(50);
-        this.image.setFitWidth(50);
+        var scale = IMG_LOADER.getSpriteScaleOf(objID).get();
+        this.image = new ImageView(new Image(IMG_LOADER.getSpriteFilePathOf(objID).get(),
+                IMG_LOADER.getSpritewidth(objID).get() * scale,
+                IMG_LOADER.getSpriteHeight(objID).get() * scale,
+                true,
+                true));
         this.image.setVisible(true);
         this.render(pos, angle);
     }
