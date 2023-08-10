0abdc56d678af978c7aaff3b077bbcb143f8d529
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2023-08-10 02:03:04 +0200

    Fixed ArenaDimensions to accommodate testing

--- src/main/java/it/unibo/alienenterprises/controller/bounds/ArenaDimensions.java
+++ src/main/java/it/unibo/alienenterprises/controller/bounds/ArenaDimensions.java
@@ -10,24 +10,25 @@ import javafx.stage.Screen;
  * @author Giulia Bonifazi
  */
 public class ArenaDimensions implements Dimensions {
-    private final double WIDTH = Screen.getPrimary().getBounds().getWidth();
-    private final double HEIGHT = 600.0;
+    private final double preset_width;
+    private final double preset_height = 600.0;
     private double width;
     private double height;
 
-    public ArenaDimensions(Point2D dimensions) {
-        this.width = dimensions.getX();
-        this.height = dimensions.getY();
-    }
-
     public ArenaDimensions(double width, double height) {
+        this.preset_width = 900.0;
         this.width = width;
         this.height = height;
     }
 
+    public ArenaDimensions(Point2D dimensions) {
+        this(dimensions.getX(), dimensions.getY());
+    }
+
     public ArenaDimensions() {
-        this.width = WIDTH;
-        this.height = HEIGHT;
+        this.preset_width = Screen.getPrimary().getBounds().getWidth();
+        this.width = this.preset_width;
+        this.height = this.preset_height;
     }
 
     @Override
