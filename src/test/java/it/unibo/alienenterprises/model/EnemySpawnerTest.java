da6c86693155d5b893bd60ba7b9a17158145ccb8
Alessio Lizza <alessio.lizza@studio.unibo.it>
2023-08-10 02:05:17 +0200

    Fixed hitbox and spawner test and checkstyle in EnemySpawnerImpl

--- src/test/java/it/unibo/alienenterprises/model/EnemySpawnerTest.java
+++ src/test/java/it/unibo/alienenterprises/model/EnemySpawnerTest.java
@@ -3,24 +3,31 @@ package it.unibo.alienenterprises.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
+import it.unibo.alienenterprises.controller.bounds.ArenaDimensions;
+import it.unibo.alienenterprises.model.api.GameObject;
+import it.unibo.alienenterprises.model.api.GameObjectAbs;
 import it.unibo.alienenterprises.model.api.Statistic;
 import it.unibo.alienenterprises.model.api.components.HitboxComponent;
 import it.unibo.alienenterprises.model.api.components.InputComponent;
 import it.unibo.alienenterprises.model.geometry.Point2D;
+import it.unibo.alienenterprises.model.world.GameWorld;
 /**
  * Test class for enemySpawner.
  */
 public class EnemySpawnerTest {
+    private static final Map<Statistic, Integer> statmap = Map.of(Statistic.HP, 100, Statistic.DAMAGE, 1);
+    private static final GameObject player = new GameObjectAbs(new Point2D(2, 0), null, statmap, null);
     /**
      * Test to check if the map creation happens correctly.
      */
     @Test
     public void checkMap() {
-        var world = new GameWorld();
-        var enemySpawner = new EnemySpawnerImpl(world, new Point2D(0, 0), new Point2D(10, 10));
+        var world = new GameWorld(new ArenaDimensions(10.0, 10.0));
+        var enemySpawner = new EnemySpawnerImpl(world, new Point2D(0, 0), new Point2D(10, 10), player);
         enemySpawner.update(61000);
         var map = new HashMap<Statistic, Integer>();
         map = enemySpawner.getStats();
@@ -38,9 +45,9 @@ public class EnemySpawnerTest {
      */
     @Test
     public void checkGameObject() {
-        var world = new GameWorld();
-        var enemySpawner = new EnemySpawnerImpl(world, new Point2D(0, 0), new Point2D(10, 10));
-        var enemy = enemySpawner.getEnemy();
+        var world = new GameWorld(new ArenaDimensions(10.0, 10.0));
+        var enemySpawner = new EnemySpawnerImpl(world, new Point2D(0, 0), new Point2D(10, 10), player);
+        var enemy = enemySpawner.getEnemy("enemySniper", 80);
         assertEquals(enemy, enemy.getComponent(HitboxComponent.class).get().getGameObject());
         assertEquals(enemy, enemy.getComponent(InputComponent.class).get().getGameObject());
     }
