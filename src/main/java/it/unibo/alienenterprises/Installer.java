4b659566c09e6eb1a29aed192b5ab36004efbe5d
Galileo Foschini <galileo.foschini@studio.unibo.it>
2023-08-11 17:58:38 +0200

    Fix to Installer

--- src/main/java/it/unibo/alienenterprises/Installer.java
+++ src/main/java/it/unibo/alienenterprises/Installer.java
@@ -2,7 +2,6 @@ package it.unibo.alienenterprises;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -58,11 +57,13 @@ public final class Installer {
         }
         if (!doesFileExist(PASSWORD_FILE)) {
             final File passwordFile = new File(DIRECTORY_PATH + SEP + PASSWORD_FILE);
-            try (FileWriter writer = new FileWriter(passwordFile)) {
+            try {
+                passwordFile.createNewFile();
                 LOGGER.info("password file succesfully created at:" + DIRECTORY_PATH);
             } catch (final IOException e) {
                 LOGGER.error("Cannot create password file:", e);
             }
+
         }
     }
 
